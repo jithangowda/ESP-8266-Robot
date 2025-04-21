bd906804481e26f4336ec62786316b582f094137
jithangowda <jithangck714@gmail.com>
2025-04-21 21:37:25 +0530

    servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,57 @@
 #include <Arduino.h>
 #include <ESPConnection.h>
+#include <Servo.h>
+
+#define CONTROL_PORT 4212
 
 ESPConnection connection("ESP8266_Robot", "robot123");
+WiFiUDP controlUdp;
+
+Servo panServo;
 
 void setup()
 {
   connection.begin();
+  controlUdp.begin(CONTROL_PORT);
+
+  panServo.attach(13);
+  Serial.println("Setup complete");
+}
+
+void checkControlData()
+{
+  char buf[64];
+  int size = controlUdp.parsePacket();
+  if (size)
+  {
+    int len = controlUdp.read(buf, sizeof(buf) - 1);
+    if (len > 0)
+    {
+      buf[len] = '\0';
+      String msg = String(buf);
+      Serial.print("[ESP8266] Control data: ");
+      Serial.println(msg);
+
+      int panIndex = msg.indexOf("PAN:");
+
+      if (panIndex != -1)
+      {
+        // Extract PAN value
+        String panVal = msg.substring(panIndex + 4);
+        int pan = panVal.toInt();
+
+        Serial.printf("Parsed ➜ PAN: %d\n", pan);
+
+        int panMapped = map(pan, 0, 180, 40, 160);
+
+        panServo.write(panMapped);
+      }
+    }
+  }
 }
 
 void loop()
 {
-  connection.listenAndHandle(); // New unified function to manage server + notification
+  connection.listenAndHandle();
+  checkControlData();
 }
