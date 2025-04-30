d132aeb45e83dd1838f05dc72600f27187005632
jithangowda <jithangck714@gmail.com>
2025-04-30 20:46:09 +0530

    pan cp

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@ ESPConnection connection("ESP8266_Robot", "robot123");
 WiFiUDP controlUdp;
 
 Servo tiltServo;
+Servo panServo;
 
 void setup()
 {
   connection.begin();
   controlUdp.begin(CONTROL_PORT);
 
-  tiltServo.attach(2);
+  tiltServo.attach(2); // Tilt servo on D2 (GPIO4)
+  panServo.attach(13); // Pan servo on D7 (GPIO13)
   Serial.println("Setup complete");
 }
 
@@ -32,8 +34,8 @@ void checkControlData()
       Serial.print("[ESP8266] Control data: ");
       Serial.println(msg);
 
+      // TILT
       int tiltIndex = msg.indexOf("TILT:");
-
       if (tiltIndex != -1)
       {
         String tiltVal = msg.substring(tiltIndex + 5);
@@ -42,6 +44,27 @@ void checkControlData()
         Serial.printf("Parsed ➜ TILT: %d\n", tilt);
 
         tiltServo.write(tilt);
+      }
+
+      // PAN
+      int panIndex = msg.indexOf("PAN:");
+      if (panIndex != -1)
+      {
+        String panDirection = msg.substring(panIndex + 4);
+        Serial.printf("Parsed ➜ PAN: %s\n", panDirection.c_str());
+
+        if (panDirection == "right")
+        {
+          panServo.write(40);
+        }
+        else if (panDirection == "left")
+        {
+          panServo.write(140);
+        }
+        else if (panDirection == "stop")
+        {
+          panServo.write(90);
+        }
       }
     }
   }
@@ -52,3 +75,7 @@ void loop()
   connection.listenAndHandle();
   checkControlData();
 }
+
+// Servo 1 - pan ( horizontal) - D7 - gpio13  (360 deg servo)
+
+// Servo 2 - tilt (vertical) - D4 - gpio2 ( mid 90, up 0)
